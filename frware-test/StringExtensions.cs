0b83dbeeec1c3959c5cf6bc84df8c9bb67f60ef2
FurC0de <prograsteve@gmail.com>
2022-12-03 20:49:44 +0300

    Added string extensions for converting to DrawingChar[]

---
+++ frware-test/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace frware_test
+{
+    internal static class StringExtensions
+    {
+        public static DrawingChar[] ToDrawingCharArray(this string s)
+        {
+            return s.ToDrawingCharArray("#FFFFFF");
+        }
+
+        public static DrawingChar[] ToDrawingCharArray(this string s, string color)
+        {
+            DrawingChar[] drawingChars = new DrawingChar[s.Length];
+
+            for (int i = 0; i<s.Length; i++)
+                drawingChars[i] = new DrawingChar(s[i], color);
+
+            return drawingChars;
+        }
+    }
+}
